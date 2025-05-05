4e4e1e56bd6f05e2135935be465d23c7bb9240fc
Axorn <oskidem@gmail.com>
2025-05-05 21:43:47 +0200

    Aktualizacja frontendu

--- frontend/color.js
+++ frontend/color.js
@@ -4,8 +4,20 @@ export class Color {
     b;
     clamped;
 
-    static fromVar(val) {
-        return Color.fromHex(getComputedStyle(document.body).getPropertyValue(val));
+    // static fromRGB
+    constructor({r, g, b}) {
+        this.r = Color.clamp(r, 0, 255);
+        this.g = Color.clamp(g, 0, 255);
+        this.b = Color.clamp(b, 0, 255);
+        this.clamped = r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0;
+    }
+
+    static clamp(v, min, max) {
+        return Math.max(Math.min(v, max), min);
+    }
+
+    static gammaToLinear(c) {
+        return c >= 0.04045 ? Math.pow((c + 0.055) / 1.055, 2.4) : c / 12.92;
     }
 
     static fromHexNum(hex) {
@@ -16,7 +28,12 @@ export class Color {
         return Color.fromHexNum(parseInt(hex.replace('#', ''), 16));
     }
 
+    static fromVar(val) {
+        return Color.fromHex(getComputedStyle(document.body).getPropertyValue(val));
+    }
+
     get hexNum() {
         return (Math.round(this.r) << 16) + (Math.round(this.g) << 8) + Math.round(this.b);
     }
 }
+
