96fd91d28e3bda868f37f9e0960b0ff76c0f04da
Łukasz Michalak <lupinummail@gmail.com>
2025-05-06 10:28:49 +0200

    Rozszerzenie klasy Tile o obsługę koloru, nazwy i opisów
    - Dodano nowe właściwości: `color`, `name`, `desc_left`, `desc_right` do klasy Tile.
    - Rozszerzono konstruktor, `redraw()` i `update()` o obsługę nowych danych.
    - W metodzie `update()` dodano rysowanie tła kafelka w kolorze oraz ciemniejszego paska u dołu.
    - Dodano wyświetlanie nazwy koloru, kodu HEX oraz opcjonalnych opisów po lewej i prawej stronie.
    - Zachowano renderowanie pustego kafelka w przypadku braku koloru.

--- frontend/color.js
+++ frontend/color.js
@@ -12,9 +12,15 @@ export class Color {
         this.clamped = r > 255 || g > 255 || b > 255 || r < 0 || g < 0 || b < 0;
     }
 
+    static linearToGamma(c) {
+        return c >= 0.0031308 ? 1.055 * Math.pow(c, 1 / 2.4) - 0.055 : 12.92 * c;
+    }
+
     static clamp(v, min, max) {
         return Math.max(Math.min(v, max), min);
     }
+
+
 
     static gammaToLinear(c) {
         return c >= 0.04045 ? Math.pow((c + 0.055) / 1.055, 2.4) : c / 12.92;
@@ -22,6 +28,17 @@ export class Color {
 
     static fromHexNum(hex) {
         return new Color({r: (hex >> 16) & 0xff, g: (hex >> 8) & 0xff, b: hex & 0xff});
+    }
+
+    static fromOklab({L, a, b}) {
+        const l = (L + a * +0.3963377774 + b * +0.2158037573) ** 3;
+        const m = (L + a * -0.1055613458 + b * -0.0638541728) ** 3;
+        const s = (L + a * -0.0894841775 + b * -1.2914855480) ** 3;
+        return new Color({
+            r: Color.linearToGamma(l * +4.0767416621 + m * -3.3077115913 + s * +0.2309699292) * 255,
+            g: Color.linearToGamma(l * -1.2684380046 + m * +2.6097574011 + s * -0.3413193965) * 255,
+            b: Color.linearToGamma(l * -0.0041960863 + m * -0.7034186147 + s * +1.7076147010) * 255
+        });
     }
 
     static fromHex(hex) {
@@ -34,6 +51,35 @@ export class Color {
 
     get hexNum() {
         return (Math.round(this.r) << 16) + (Math.round(this.g) << 8) + Math.round(this.b);
+    }
+
+    get hex() {
+        return '#' + this.hexNum.toString(16);
+    }
+
+    get barColor() {
+        const {L, a, b} = this.oklab;
+        return Color.fromOklab({L: L * 0.9, a, b});
+    }
+
+    get textColor() {
+        const {L, a, b} = this.oklab;
+        const Ln = L <= 0.483 ? L * 0.9 + 0.3 : L * 0.75 - 0.15;
+        return Color.fromOklab({L: Ln, a, b});
+    }
+
+    get oklab() {
+        const r = Color.gammaToLinear(this.r / 255);
+        const g = Color.gammaToLinear(this.g / 255);
+        const b = Color.gammaToLinear(this.b / 255);
+        const l = Math.cbrt(0.4122214708 * r + 0.5363325363 * g + 0.0514459929 * b);
+        const m = Math.cbrt(0.2119034982 * r + 0.6806995451 * g + 0.1073969566 * b);
+        const s = Math.cbrt(0.0883024619 * r + 0.2817188376 * g + 0.6299787005 * b);
+        return {
+            L: l * +0.2104542553 + m * +0.7936177850 + s * -0.0040720468,
+            a: l * +1.9779984951 + m * -2.4285922050 + s * +0.4505937099,
+            b: l * +0.0259040371 + m * +0.7827717662 + s * -0.8086757660
+        };
     }
 }
 
--- frontend/settings.js
+++ frontend/settings.js
@@ -6,11 +6,29 @@ export class Settings {
     update({
                grid_height = 168,
                grid_width = 224,
-
+               bar_height = 10,
+               name_offset = -10,
+               hex_offset = 35,
+               hex_offset_nameless = 0,
+               desc_offset_x = 15,
+               desc_offset_y = 20,
+               name_size = 40,
+               hex_size = 26,
+               hex_size_nameless = 34,
+               desc_size = 26,
            }) {
         this.grid_height = grid_height;
         this.grid_width = grid_width;
-
+        this.bar_height = bar_height;
+        this.name_offset = name_offset;
+        this.hex_offset = hex_offset;
+        this.hex_offset_nameless = hex_offset_nameless;
+        this.desc_offset_x = desc_offset_x;
+        this.desc_offset_y = desc_offset_y;
+        this.name_size = name_size;
+        this.hex_size = hex_size;
+        this.hex_size_nameless = hex_size_nameless;
+        this.desc_size = desc_size;
         return this;
     }
 }
